bea951a4b14e317b334768c418062e3b044fb93a
carloseeyah <b2029cmrsia@pshs.edu.ph>
2025-08-05 08:49:05 +0800

    First Non-Graded

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// no script tag needed
+let presentAge = 14;
+let maxAge = 90;
+let numPerDay = 2;
+let totalRequired = (numPerDay * 365)*(maxAge-presentAge);
+
+document.write("You will need " + totalRequired + " Biscoff Biscuits to last you until the ripe old age of " + maxAge + ".");
+document.write
